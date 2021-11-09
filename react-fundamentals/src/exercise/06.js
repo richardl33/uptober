7c072046194ddc31fdb5b8a7888e35064c4a846b
Richard Lee <richardlee@flashtract.com>
2021-11-09 11:02:19 -0500

    ex 06 - forms

--- react-fundamentals/src/exercise/06.js
+++ react-fundamentals/src/exercise/06.js
@@ -9,21 +9,31 @@ function UsernameForm({onSubmitUsername}) {
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const value = event.target.elements.usernameInput.value;
+    onSubmitUsername(value);
+  }
+
   //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
+  // const onSubmitUsername = (event) => {
+
+  // }
 
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>

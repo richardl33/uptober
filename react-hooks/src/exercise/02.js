d1b00766b41c32000491036a4215acb8eba80c40
Richard Lee <rich.leeroy@gmail.com>
2021-11-14 13:19:54 -0500

    useState lazy state init

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -6,9 +6,15 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') || initialName
-  )
+  // const [name, setName] = React.useState(
+  //   window.localStorage.getItem('name') || initialName
+  // )
+
+  const getInitialNameValue = () => { 
+    return window.localStorage.getItem('name') || initialName 
+  };
+
+  const [name, setName] = React.useState(getInitialNameValue())
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
